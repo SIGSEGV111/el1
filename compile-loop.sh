b093f44ca13f9f61d75e3e8e4c12af75abfb1032
Simon Brennecke <simon@brennecke-it.net>
2023-10-30 18:55:12 +0100

    compile-loop.sh

--- compile-loop.sh
+++ compile-loop.sh
@@ -5,7 +5,7 @@ parseCommandlineArguments "f:follow" "t:target=string?dev" "j:jobs:NUMCPUS=integ
 less_args=("--RAW-CONTROL-CHARS" "--clear-screen" "--tabs=4" "--ignore-case" "--SILENT")
 
 if (($__follow)); then
-	less_args+=("+F")
+	less_args+=("--exit-follow-on-close" "+F")
 else
 	less_args+=("--quit-on-intr")
 fi
@@ -23,6 +23,7 @@ trap on_exit EXIT SIGINT SIGHUP SIGQUIT SIGTERM
 trap "" ERR
 set +e +o pipefail +o errtrace
 
+export GTEST_COLOR=yes
 while true; do
 	make -j $__jobs "$__target" 2>&1 | less "${less_args[@]}"
 	sleep 0.5
